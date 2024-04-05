7f54c6a8432577d3427a740d1307cde637355cd9
JonasBorgesLM <jonasleo92@yahoo.com.br>
2024-04-05 11:36:49 -0300

    estudando Sonarqube na nuvem com go

---
+++ sum.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func multiplication(a, b int) int {
+	return a * b
+}
